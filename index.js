065414a7628113b84b5baa39080ea258e8b3b59a
Tanuka Das <das.tanuka@yahoo.com>
2019-03-24 22:10:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function add2(n) {
   // Feel free to move things around!
   const two = 2
   return n + two;
-  //console.log(n);
+  console.log(n);
 }
 //add2();
 //--------------------------------------------------------------------->
